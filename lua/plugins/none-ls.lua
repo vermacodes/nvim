d7d0fa67a029f3360d33119a07b986f0b5a0562a
vermacodes <19263858+vermacodes@users.noreply.github.com>
2024-03-14 23:25:34 -0400

    Initial Commit

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
     "nvimtools/none-ls-extras.nvim",
   },
   config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				require("none-ls.diagnostics.eslint_d"),
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+  end,
 }
